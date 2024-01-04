64c8e07086429dd6f6e56905f6ee93e262db9811
Daniel Marulanda <d@dondo.com>
2024-01-04 14:20:47 -0500

    Geobristol Bot

--- src/handlers/langchain.ts
+++ src/handlers/langchain.ts
@@ -1,6 +1,5 @@
 import { Message } from "whatsapp-web.js";
 import * as cli from "../cli/ui";
-import { fullChain } from "../database";
 
 
 // TODO add conversation ID to build a chat history
@@ -9,17 +8,12 @@ const handleMessageLangChain = async (message: Message, prompt: string) => {
 
 		const start = Date.now();
 		  
-		const output = await fullChain.invoke({
-			question: prompt,
-		  });
-
-		console.log(output)
+		console.log(message)
 
 		const end = Date.now() - start;
 
-		cli.print(`[GPT] Answer to ${message.from}: ${output}  | OpenAI request took ${end}ms)`);
+		cli.print(`[GPT] Answer to ${message.from}: ${message}  | OpenAI request took ${end}ms)`);
 		// Default: Text reply
-		message.reply(String(output.content));
 	} catch (error: any) {
 		console.error("An error occured", error);
 		message.reply("An error occured, please contact the administrator. (" + error.message + ")");
